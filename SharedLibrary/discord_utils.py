3347cb44f941bb8921a1c1ccf369513acb974a9d
Bruno Dantas <bruno.d.paiva@hotmail.com>
2021-02-28 01:17:04 -0300

    [SharedLibrary] Adding functions docstring.

--- SharedLibrary/discord_utils.py
+++ SharedLibrary/discord_utils.py
@@ -7,6 +7,15 @@ bot_icon = "https://cdn.discordapp.com/attachments/539836343094870016/8130961687
 
 
 def is_dm(ctx, dm_channel):
+    """ Checks if the user is sending a message on dm
+
+    Args:
+        ctx (object): object related to the user
+        dm_channel (object): object that refers to dm
+
+    Returns:
+        bool: true if the user send message on dm
+    """
     if isinstance(ctx.channel, dm_channel):
         return True
 
@@ -14,10 +23,27 @@ def is_dm(ctx, dm_channel):
 
 
 def is_valid_document(doc_type):
+    """ Checks if the doc_type parameter is valid
+
+    Args:
+        doc_type (str): document type to be downloaded
+
+    Returns:
+        bool: True if it's valid
+    """
     if doc_type.lower() not in list_of_documents:
         return False
     return True
 
 
 def similar(a, b):
+    """ Checks how similar is a str from another
+
+    Args:
+        a (str): first string to be compared
+        b (str): second string to be compared
+
+    Returns:
+        float: ratio in how similar are the strings
+    """
     return SequenceMatcher(None, a, b).ratio()
--- SharedLibrary/siga_utils.py
+++ SharedLibrary/siga_utils.py
@@ -11,12 +11,29 @@ list_of_documents = {'historico': {'botaoHistorico': 'botaoHistorico'},
 
 
 def get_enrolled_page_token(html_content):
+    """ Get the page token
+
+    Args:
+        html_content (str): the page html
+
+    Returns:
+        str: page token
+    """
     soup = BeautifulSoup(html_content, "html.parser")
     # We need a token to get the raw json enrollment data
     return soup.find("span", {"id": "token"}).text
 
 
 def get_login_post_data(username, password, html_content):
+    """ Generate the post data to be used on login
+
+    Args:
+        username (str): login (usually the cpf)
+        password (str): password to access the siga
+        html_content (str): the page html
+    Returns:
+        dictionary: post data
+    """
     postData = {'authenticity_token': get_authenticity_token_parameter(html_content),
                 'lt': get_lt_parameter(html_content),
                 'username': username,
@@ -26,16 +43,39 @@ def get_login_post_data(username, password, html_content):
 
 
 def get_authenticity_token_parameter(html_content):
+    """ Get the authenticity token
+
+    Args:
+        html_content (str): the page html
+
+    Returns:
+        str: authenticity token
+    """
     soup = BeautifulSoup(html_content, "html.parser")
     return soup.find("input", {"name": "authenticity_token"})["value"]
 
 
 def get_lt_parameter(html_content):
+    """ Get the lt parameter
+
+    Args:
+        html_content (str): the page html
+
+    Returns:
+        str: lt parameter
+    """
     soup = BeautifulSoup(html_content, "html.parser")
     return soup.find("input", {"id": "lt"})["value"]
 
 
 def siga_document_post_data(doc_type):
+    """ Generate the post data to be used on pdf download
+
+    Args:
+        doc_type (str): document to be downloaded in the page
+    Returns:
+        dictionary: post data
+    """
     return {
         "gnosys-decor-vis-seletor-matricula-aluno": "0",
         "gnosys-decor-vis-seletor-matricula-form": "gnosys-decor-vis-seletor-matricula-form",
@@ -45,6 +85,14 @@ def siga_document_post_data(doc_type):
 
 
 def treat_login(login):
+    """ Checks if the login is a valid cpf
+
+    Args:
+        login (str): siga login
+
+    Returns:
+        str: cpf digits
+    """
     if(not CPF().validate(login)):
         return ""
     return login.replace(".", "").replace("-", "")
